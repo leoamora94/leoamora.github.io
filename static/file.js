bec139ffa2a1d6c47bba34d2e75ee95c52ef2c9e
Leo Amora <leoamora94@gmail.com>
2023-12-15 18:15:26 -0300

    changes index.html and .css, add .js

---
+++ static/file.js
@@ -0,0 +1,35 @@
+// Click events for buttons
+const portfolio = document.getElementById("portfolio");
+const portfolioBtn = document.getElementById("portfolio-btn");
+const skills = document.getElementById("skills");
+const skillsBtn = document.getElementById("skills-btn");
+const certificates = document.getElementById("certificate");
+const certificateBtn = document.getElementById("certificate-btn");
+
+portfolioBtn.addEventListener("click", (event) => {
+  skills.style.display = "none";
+  certificates.style.display = "none";
+  portfolio.style.display = "flex";
+  skillsBtn.classList.remove("active-btn");
+  portfolioBtn.classList.add("active-btn");
+  certificateBtn.classList.remove("active-btn");
+});
+
+skillsBtn.addEventListener("click", (event) => {
+  skills.style.display = "flex";
+  portfolio.style.display = "none";
+  certificates.style.display = "none";
+  portfolioBtn.classList.remove("active-btn");
+  skillsBtn.classList.add("active-btn");
+  certificateBtn.classList.remove("active-btn");
+});
+
+
+certificateBtn.addEventListener("click", (event) => {
+  certificates.style.display = "flex";
+  skills.style.display = "none";
+  portfolio.style.display = "none";
+  skillsBtn.classList.remove("active-btn");
+  portfolioBtn.classList.remove("active-btn");
+  certificateBtn.classList.add("active-btn");
+});
